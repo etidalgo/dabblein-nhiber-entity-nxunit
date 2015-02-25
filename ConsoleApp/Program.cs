4b481558dc15864146d1cfdecd27ba932dde64f8
etidalgo <etidalgo@sbs.net.nz>
2015-02-26 11:38:41 +1300

    Clean up. Add project for EntityFramework. Add consoleapp.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using MyCompany.Domain;
+using MyCompany.EF;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ProductContext())
+            {
+                // Create and save a new Product 
+                Console.Write("Enter a name for a new product: ");
+                var name = Console.ReadLine();
+
+                var product = new Product { Name = name, Category = "Category" };
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                // Display all Products from the database 
+                var query = from b in db.Products
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All products in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            } 
+        }
+    }
+
+
+}
---
+++ MyCompany-EFdbcontext/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyCompany.Domain;
+using System.Data.Entity;
+
+namespace MyCompany.EF
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
